81c29062f70550a04c258090811e01bfa64dbb1b
MugeraH <bwosihughes24@gmail.com>
2021-05-04 19:00:30 +0300

    update initial configuration files and basic application setup

---
+++ app/main/views.py
@@ -0,0 +1,9 @@
+from flask import render_template,request,redirect,url_for,request,redirect,url_for
+from . import main
+
+
+
+@main.route('/')
+def index():
+   
+    return render_template('index.html')
---
+++ config.py
@@ -0,0 +1,22 @@
+import os
+
+class Config:
+    pass
+    # we change to our own databases 
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mugera:Mugbwo9856@localhost/letewa'
+
+    
+
+
+class ProdConfig(Config):
+    pass
+    # SQLALCHEMY_DATABASE_URI = "postgresql://vzptjkvwwukqax:9f863d965fec49851c9daa903a53bb7d5acb267c1ea1d19ea0012b73ff9e1f65@ec2-34-225-167-77.compute-1.amazonaws.com:5432/ddjuf4iqfo6c55?sslmode=require"
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
