5f1f4aec8a7e17ae5cc454d0688c5a27db16236b
MugeraH <bwosihughes24@gmail.com>
2021-05-07 10:56:27 +0300

    add profile section

--- app/main/views.py
+++ app/main/views.py
@@ -260,6 +260,18 @@ def update_products():
 
 
 #Routes for user profile change
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+  
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
+
 @main.route('/user/<uname>/update',methods = ['GET','POST'])
 @login_required
 def update_profile(uname):
@@ -293,32 +305,32 @@ def update_pic(uname):
 
 #Routes for supplier profile change
 
-@main.route('/supplier/<uname>/update',methods = ['GET','POST'])
-@login_required
-def update_supplier_profile(uname):
-    user = Supplier.query.filter_by(username = uname).first()
-    if user is None:
-        abort(404)
+# @main.route('/supplier/<uname>/update',methods = ['GET','POST'])
+# @login_required
+# def update_supplier_profile(uname):
+#     user = Supplier.query.filter_by(username = uname).first()
+#     if user is None:
+#         abort(404)
 
-    form = UpdateProfile()
+#     form = UpdateProfile()
 
-    if form.validate_on_submit():
-        user.bio = form.bio.data
+#     if form.validate_on_submit():
+#         user.bio = form.bio.data
 
-        db.session.add(user)
-        db.session.commit()
+#         db.session.add(user)
+#         db.session.commit()
 
-        return redirect(url_for('.profile',uname=user.username))
+#         return redirect(url_for('.profile',uname=user.username))
 
-    return render_template('profile/update.html',form =form)
+#     return render_template('profile/update.html',form =form)
 
-@main.route('/supplier/<uname>/update/pic',methods= ['POST'])
-@login_required
-def update_supplier_pic(uname):
-    user = Supplier.query.filter_by(username = uname).first()
-    if 'photo' in request.files:
-        filename = photos.save(request.files['photo'])
-        path = f'photos/{filename}'
-        user.profile_pic_path = path
-        db.session.commit()
-    return redirect(url_for('main.profile',uname=uname))
+# @main.route('/supplier/<uname>/update/pic',methods= ['POST'])
+# @login_required
+# def update_supplier_pic(uname):
+#     user = Supplier.query.filter_by(username = uname).first()
+#     if 'photo' in request.files:
+#         filename = photos.save(request.files['photo'])
+#         path = f'photos/{filename}'
+#         user.profile_pic_path = path
+#         db.session.commit()
+#     return redirect(url_for('main.profile',uname=uname))
